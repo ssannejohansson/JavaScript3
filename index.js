1b463ee292324c102c5de4e4ed839b3eb4065e5f
ssannejohansson <sannej0hansson@hotmail.com>
2025-03-25 09:41:55 +0100

    Finished assignment
    Finished assignment

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
 // Assignment 3 
+// min 8 char, no space, excl username
+
+
+let password = "abcdefgh";
+let username = "sannejohansson"; 
+
+function isValidPassword(password) {
+    const spaceRegex = /\s/;
+    
+    if (password.length < 8) {
+        return false; 
+    }
+
+    if (spaceRegex.test(password)) {
+        return false;
+    }
+
+    if (password.includes(username)) {
+        return false;
+    }
+
+    return true; 
+}
+
+console.log(isValidPassword("abcdefgh"));
+
 
